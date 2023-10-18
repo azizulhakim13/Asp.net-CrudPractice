6bcd4e21c18f1315662a06efa0b16e97b72518df
azizulhakim13 <mah.azizulhakim.sust@gmail.com>
2023-10-18 17:01:57 +0600

    Add project files.

---
+++ CrudPractice/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudPractice.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudPractice.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
